5b40fc207e868ab3ca926c6001bd5cc539ce3259
hurshidbe <egamberdiyevx8@gmail.com>
2025-07-19 13:29:06 +0500

    add: swagger added to project

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe, ArgumentsHost } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swagger = new DocumentBuilder()
+    .setTitle('Hr_backend')
+    .setDescription('Hr_project all endpoints')
+    .setVersion('1.0')
+    .addCookieAuth('authToken')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('apis', app, document);
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
